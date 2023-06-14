308261f8882967ad9f9c5d6e90edbad2dfa2933a
IsmaelAsimbaya <85204681+IsmaelAsimbaya@users.noreply.github.com>
2023-06-14 02:18:29 -0500

    implementacion de crud

--- src/main/java/com/distribuida/GreetingResource.java
+++ src/main/java/com/distribuida/GreetingResource.java
@@ -1,10 +1,10 @@
 package com.distribuida;
 
 import com.distribuida.db.Persona;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 import java.util.List;
 
@@ -19,19 +19,46 @@ class Message {
         this.msg = msg;
     }
 }
-@Path("/hello")
+@Path("/personas")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class GreetingResource {
 
-    /*@GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Message hello(){
-        return new Message();
-    }*/
-
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Persona> hello(){
+    public List<Persona> getAllPersonas() {
         return Persona.listAll();
+    }
+
+    @POST
+    @Transactional
+    public Response createPersona(Persona persona) {
+        Persona.persist(persona);
+        return Response.ok(persona).status(Response.Status.CREATED).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Transactional
+    public Response updatePersona(@PathParam("id") Long id, Persona personaData) {
+        Persona persona = Persona.findById(id);
+        if (persona == null) {
+            throw new NotFoundException("Persona con ID " + id + " no encontrada");
+        }
+        persona.setName(personaData.getName());
+        persona.persist();
+        return Response.ok(persona).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public Response deletePersona(@PathParam("id") Long id) {
+        Persona persona = Persona.findById(id);
+        if (persona == null) {
+            throw new NotFoundException("Persona con ID " + id + " no encontrada");
+        }
+        persona.delete();
+        return Response.noContent().build();
     }
 
 }
